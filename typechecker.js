c80d4f67974db438f60a6c9d700af33acf197226
Your Name <josh.e.fielding@gmail.com>
2019-01-10 07:42:18 +1100

    morning work

--- typechecker.js
+++ typechecker.js
@@ -8,7 +8,23 @@ const lineheightTag = document.querySelector(`input[name="lineheight"]`)
 
 const lineheightOutput = document.querySelector("span.lineheight-output")
 
+// for the font-weight
+const fontweightTag = document.querySelector(`input[name="fontweight"]`)
+
+// for the font-weight
+const fontweightOutput = document.querySelector("span.fontweight-output")
+
 const italicTag = document.querySelector(`input[name="italic"]`)
+
+const typefaceTag = document.querySelector(`select[name="typeface"]`)
+
+
+
+// for the different colour backgrounds on the a
+// querySelectorAll because there's multiple colours, not just querySelector
+const colorTags = document.querySelectorAll("div.colors div")
+
+
 
 const outputTag = document.querySelector("textarea.output")
 
@@ -46,6 +62,14 @@ typesizeTag.addEventListener("input", function () {
 })
 
 
+// change my font-weight using the fontweightTag and fontweightOutput
+
+fontweightTag.addEventListener("input", function () {
+  outputTag.style.fontWeight = this.value
+  fontweightOutput.innerHTML = this.value
+})
+
+
 lineheightTag.addEventListener("input", function () {
 
   outputTag.style.lineHeight = this.value
@@ -61,11 +85,41 @@ italicTag.addEventListener("change", function () {
     // pick the output tag then change the style then find the font style
     outputTag.style.fontStyle = "italic"
 
-  } 
+  }
   // doesn't need the curly brackets because already connected to the if statement
-      else {
-        outputTag.style.fontStyle = "normal"
+  else {
+    outputTag.style.fontStyle = "normal"
 
   }
+
+})
+
+// when I change my select for typeface, update the font family
+typefaceTag.addEventListener("input", function () {
+  outputTag.style.fontFamily = this.value
+})
+
+
+// go through all of my colour tags, then ...
+// when I click one of them, change the background colour and the text colour, and make this tag be selected
+
+// 1. load all the color tags
+colorTags.forEach(tag => {
+  
+  // 2. wait for a click, then change the background colour and text 
+  tag.addEventListener("click", function () {
+    outputTag.style.backgroundColor = this.style.backgroundColor
+    outputTag.style.color = this.style.color
+
+    // reset the classes
+    // 3. then go through all the previous tags and remove the class .selected
+    colorTags.forEeach(tag => {
+      tag.classList.remove("selected")
+    })
+    
+    // 4. then only add the class .selected to this clicked one
+    this.classList.add("selected")
+
+  })
 
 })
